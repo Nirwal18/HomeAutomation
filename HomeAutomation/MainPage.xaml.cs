6c7ade0dc4adb23976f0cb0526928d2cae683725
Nirwal <nirwal@live.com>
2017-02-20 18:41:46 +0530

    Fram navigation implemented

--- HomeAutomation/MainPage.xaml.cs
+++ HomeAutomation/MainPage.xaml.cs
@@ -29,12 +29,22 @@ namespace HomeAutomation
         public MainPage()
         {
             this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            Frame_container.Navigate(typeof(Page1));
+          //  throw new NotImplementedException();
         }
 
         private void hamburger_menu_click(object sender, RoutedEventArgs e)
         {
             slider.IsPaneOpen = !slider.IsPaneOpen;
+           
         }
+
+
         /*    private async void button_Click(object sender, RoutedEventArgs e)
              {
                  string[] requestedProperties = new string[] { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected" };
@@ -49,6 +59,43 @@ namespace HomeAutomation
                  }
 
              }*/
+
+            /// <summary>
+            /// to show Status bar warnning, sucess and Error state. Used in function StatsBar();
+            /// </summary>
+        enum barStatus { Error,Warnning,Sucess,Normal};
+
+
+        /// <summary>
+        /// it is used to set property to bar.
+        /// this function is used to set text and color on Status bar. Example : 
+        /// ststus =barStatus.Error    red, 
+        /// ststus =barStatus.warning  Yellow, 
+        /// ststus =barStatus.sucess   Green 
+        /// </summary>
+        void StatusBar(string message,barStatus status)
+        {
+            switch (status)
+            {
+                case barStatus.Error:
+                    StatusBoder.Background = new SolidColorBrush(Windows.UI.Colors.DarkRed);
+                    break;
+                case barStatus.Sucess:
+                    StatusBoder.Background = new SolidColorBrush(Windows.UI.Colors.Green);
+                    break;
+                case barStatus.Warnning:
+                    StatusBoder.Background = new SolidColorBrush(Windows.UI.Colors.Yellow);
+                    break;
+            }
+
+            StatusBar_Text.Text = message;
+               
+        }
+
+        private void page2_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Frame_container.Navigate(typeof(Page2));
+        }
     }
 
 }
---
+++ HomeAutomation/Model/Btdevice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.Model
+{
+   public class Btdevice
+    {
+        public string Name { get; set; }
+        public int id { get; set; }
+    }
+
+    public class DeviceManager
+    {
+       public static List<Btdevice> GetDevice()
+        {
+            var device = new List<Btdevice>();
+            device.Add(new Btdevice { Name = "phone 1", id = 0001 });
+            device.Add(new Btdevice { Name = "phone 2", id = 0002 });
+            device.Add(new Btdevice { Name = "phone 3", id = 0003 });
+            device.Add(new Btdevice { Name = "phone 4", id = 0004 });
+            device.Add(new Btdevice { Name = "phone 5", id = 0005 });
+            device.Add(new Btdevice { Name = "phone 6", id = 0006 });
+            return device;
+        }
+
+    }
+
+}
